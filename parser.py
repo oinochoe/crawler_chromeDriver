7d6e6fe11093ac673d3668a5326da6d8168d16c3
yeongminkim <oinochoe@naver.com>
2020-08-02 18:02:15 +0900

    new test

--- parser.py
+++ parser.py
@@ -2,17 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import os
+import timeit
+
+start_time = timeit.default_timer()
+
+sum = 0
+
+for i in range(100000000):
+    sum += i
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-req = requests.get('https://beomi.github.io/beomi.github.io_old/')
+req = requests.get('https://news.google.com/articles/CBMiMWh0dHBzOi8vd3d3LmJiYy5jb20vbmV3cy93b3JsZC11cy1jYW5hZGEtNTM2MjY1NDbSATVodHRwczovL3d3dy5iYmMuY29tL25ld3MvYW1wL3dvcmxkLXVzLWNhbmFkYS01MzYyNjU0Ng?hl=en-US&gl=US&ceid=US%3Aen')
 
 html = req.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
 my_titles = soup.select(
-    'h3 a'
+    '.story-body__list-item a'
 )
 
 data = {}
@@ -22,6 +31,9 @@ for title in my_titles:
 
 with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
     json.dump(data, json_file)
+
+terminate_time = timeit.default_timer()
+print("%fsec" % (terminate_time - start_time))
 
 header = req.headers
 
