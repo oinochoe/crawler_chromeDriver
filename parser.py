a351aabac0a4e9fd9715ccec57b52a30f73ecf57
yeongminkim <oinochoe@naver.com>
2020-08-04 02:15:05 +0900

    test 코드

--- parser.py
+++ parser.py
@@ -6,34 +6,37 @@ import timeit
 
 start_time = timeit.default_timer()
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 sum = 0
 
 for i in range(100000000):
     sum += i
 
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+req = requests.get('https://www.washingtonpost.com/us-policy/2020/08/03/congress-stimulus-coronavirus-trump/')
 
-req = requests.get('https://www.bbc.com/news/world-us-canada-53626546')
+terminate_time = timeit.default_timer()
+print("%fsec" % (terminate_time - start_time))
+
 
 html = req.text
+
 
 soup = BeautifulSoup(html, 'html.parser')
 
 my_titles = soup.select(
-    '.story-body__list-item a'
+    '.font--body.font-copy'
 )
 
 data = {}
 
 for title in my_titles:
-    data[title.text] = title.get('href')
+    data[title.text] = title.get_text()
 
 with open(os.path.join(BASE_DIR, 'result.json'), 'w+') as json_file:
     json.dump(data, json_file)
-
-terminate_time = timeit.default_timer()
-print("%fsec" % (terminate_time - start_time))
 
 header = req.headers
 
